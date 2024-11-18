48d30540bdff5fd9d43e8acd3a8ddaec10a303a5
Julian <julianalejandromontero@gmail.com>
2024-11-18 18:12:41 -0500

    Creacion de ClientMapper

--- src/main/java/com/example/PruebaTecnica/mapper/ClientMapper.java
+++ src/main/java/com/example/PruebaTecnica/mapper/ClientMapper.java
@@ -1,12 +1,16 @@
 package com.example.PruebaTecnica.mapper;
 
+import com.example.PruebaTecnica.dto.ClientDataDto;
 import com.example.PruebaTecnica.dto.ClientRequestDto;
+import com.example.PruebaTecnica.dto.ClientResponseDto;
 import com.example.PruebaTecnica.entity.ClientEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ClientMapper {
-    public ClientEntity toClientDto (ClientRequestDto clientRequestDto){
+    public ClientEntity toClientEntity (ClientRequestDto clientRequestDto){
         return ClientEntity.builder()
                 .apellido(clientRequestDto.getApellido())
                 .email(clientRequestDto.getEmail())
@@ -15,5 +19,21 @@ public class ClientMapper {
                 .fechaNacimiento(clientRequestDto.getFechaNacimiento())
                 .nombre(clientRequestDto.getNombre())
                 .tipoIdentificacion(clientRequestDto.getTipoIdentificacion()).build();
+    }
+    public ClientDataDto toClientDto (ClientEntity clientEntity){
+        return ClientDataDto.builder()
+                .apellido(clientEntity.getApellido())
+                .email(clientEntity.getEmail())
+                .nombre(clientEntity.getNombre())
+                .numeroIdentificacion(clientEntity.getNumeroIdentificacion())
+                .build();
+    }
+    public ClientResponseDto toClientResponse (ClientEntity clientEntity){
+        ClientDataDto clientDataDto = toClientDto(clientEntity);
+        return ClientResponseDto.builder()
+                .code("200")
+                .data(clientDataDto)
+                .message("")
+                .build();
     }
 }
